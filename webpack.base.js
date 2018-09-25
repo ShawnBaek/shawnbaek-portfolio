03af045853c4a53096bd548ecba909e39470accd
Shawn Baek <shawn@shawnbaek.com>
2018-09-25 22:38:16 +0900

    Setting Webpack.client.js

--- webpack.base.js
+++ webpack.base.js
@@ -1,7 +1,4 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
-    entry: './styles/styles.css',
     module: {
         rules: [
             {
--- webpack.client.js
+++ webpack.client.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// const devMode = process.env.NODE_ENV !== 'production'
 
 const config = {
     mode: 'development',
@@ -11,6 +15,28 @@ const config = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
+
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.(sa|sc|c)ss$/,
+    //             use: [
+    //               devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+    //               'css-loader',
+    //               'postcss-loader',
+    //               'sass-loader',
+    //             ],
+    //           }
+    //     ]
+    // },
+    // plugins: [
+    //     new MiniCssExtractPlugin({
+    //         // Options similar to the same options in webpackOptions.output
+    //         // both options are optional
+    //         filename: "[name].css",
+    //         chunkFilename: "[id].css"
+    //       })
+    // ]
     
 };
 
